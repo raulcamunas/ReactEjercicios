75ee639f143c39a988854033070cac533652f6f7
raulcamunas <raulcamunas369@gmail.com>
2024-04-09 13:27:34 +0200

    Mini Proyecto Rick And Morty

---
+++ Mini-Ejercicio1-Rick-y-Morty/Rick-And-Morty-Api/src/components/api/Api.jsx
@@ -0,0 +1,40 @@
+import './Api.css'
+import { useEffect, useState } from 'react'
+
+
+const Card = () => {
+
+    const [api, setApi] = useState([])
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+        .then((resp) => resp.json())
+        .then((data) => {
+            setApi(data.results)
+        })
+        .catch(() => {
+            console.error("La petición Falló")
+        })
+        
+    }, [])
+
+    return (
+        <>
+            <h1> Rick And Morty Api</h1>
+            <ol className='tarjeta'>
+                {api.map(({ id, name, image, status, origin }) => (
+                    <li key={id}>
+                        <div>
+                            <h2>{name}</h2>
+                            <img src={image} alt={name} />
+                            <p>Estado: {status}</p>
+                            <p>Origen: {origin.name}</p>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </>
+    );
+}
+
+export default Card
